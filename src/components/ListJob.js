ac89bfdf74d086b314c2239cfa8f207ee8521c13
serrasabah <69539523+serrasabah@users.noreply.github.com>
2022-08-13 11:24:27 +0300

    Update ListJob.js

--- src/components/ListJob.js
+++ src/components/ListJob.js
@@ -1,53 +1,133 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import MaterialTable from "material-table";
 import { DataGrid } from "@mui/x-data-grid";
 import { JobApi } from "./JobApi";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-const columns = [
-  {
-    field: "id",
-    headerName: "ID",
-    width: 100,
-  },
-  {
-    field: "jobName",
-    headerName: "Name",
-    width: 200,
-  },
-  {
-    field: "URL",
-    headerName: "URL",
-    width: 200,
-  },
-  {
-    field: "period",
-    headerName: "period",
-    width: 100,
-  },
-  {
-    field: "timeout",
-    headerName: "timeout",
-    width: 100,
-  },
-  {
-    field: "update",
-    headerName: "update",
-    width: 60,
-  },
-  {
-    field: "delete",
-    headerName: "delete",
-    width: 50,
-  },
-];
+import { IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete'
+import { toast } from "react-toastify";
+import EqualizerIcon from '@mui/icons-material/Equalizer';
+import Icon from '@mui/material/Icon';
 
 function ListJob() {
   const [jobs, setJobs] = useState([]);
 
   const jopApi = new JobApi();
+  const [selectionModel, setSelectionModel] = useState();
+
+  async function handleCellChange(params, newValue) {
+    const jobIndex = jobs.findIndex(job => {
+      return job.id === params.id;
+    });
+  
+    
+    const updateJobs = [... jobs];
+    updateJobs[jobIndex][params.field] = newValue;
+   setJobs(updateJobs)
+
+   const id = params.id;
+   console.log(id);
+  console.log(jobIndex);
+   console.log(updateJobs[jobIndex]);
+
+   const response = await jopApi.updateJob(id,updateJobs[jobIndex]);
+   const messageResponse = response.data;
+
+  }  
+
+    async function deleteCell(){
+    console.log("id" + selectionModel)
+    const selectedIDs = selectionModel;
+              // you can call an API to delete the selected IDs
+              // and get the latest results after the deletion
+              // then call setRows() to update the data locally here
+   // console.log("size" + selectedIDs.size)
+   
+         const response = await jopApi.deleteJob(selectedIDs);
+         const messageResponse = response.data;
+         console.log(messageResponse)
+     if (messageResponse.responseType === "ERROR") {
+      toast.error(messageResponse.message);
+       console.log(messageResponse)
+   }
+   else{
+    toast.warning(messageResponse.message);
+    console.log(messageResponse)
+   }
+    
+    const newState = [...jobs]  ;
+    // const messageResponse = response.data;
+   // setJobs((r) => r.filter((x) => !selectedIDs.has(x.id)));
+    console.log(jobs);
+    console.log(selectedIDs);
+}
+
+
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      width: 100,
+      editable: false,
+    },
+    {
+      field: "jobName",
+      headerName: "Name",
+      width: 200,
+      editable: true,
+    },
+    {
+      field: "URL",
+      headerName: "URL",
+      width: 200,
+      editable: false,
+    },
+    {
+      field: "period",
+      headerName: "period",
+      width: 100,
+      editable: true,
+    },
+    {
+      field: "timeout",
+      headerName: "timeout",
+      width: 100,
+      editable: true,
+    },
+    {
+      field: "delete",
+            width: 75,
+            //sortable: false,
+            disableColumnMenu: true,
+            renderHeader: () => {
+              return (
+                <IconButton
+                  onClick={deleteCell}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              );
+            }
+          },
+          {
+            field: "view",
+                  width: 75,
+                  //sortable: false,
+                  disableColumnMenu: true,
+                  renderHeader: () => {
+                    return (
+                      <IconButton
+                        onClick={chartCell}
+                      >
+              
+                      <EqualizerIcon color="secondary" />
+                      </IconButton>
+                    );
+                  }
+                }
+  ];
+  
 
   async function getJobs() {
     const response = await jopApi.getJobs();
@@ -56,6 +136,7 @@ function ListJob() {
 
   useEffect(() => {
     getJobs();
+   // deleteCell();
   }, []);
 
   const navigate = useNavigate();
@@ -68,50 +149,55 @@ function ListJob() {
     e.preventDefault();
     navigate("/AddJob");
   }
-  console.log(columns.findIndex);
   return (
-    // <Box
-    //   sx={{
-    //     height: "450px",
-    //     width: "120vh",
-    //     margin: "20vh",
-    //     marginLeft: "50vh",
-    //   }}
-    // >
-    //   <DataGrid
-    //     rows={jobs}
-    //     columns={columns}
-    //     pageSize={5}
-    //     rowsPerPageOptions={[5]}
-    //     checkboxSelection
-    //     disableSelectionOnClick
-    //   />
-    //   <div className="create-button">
-    //     <button onClick={returnHomePage}>Home</button>
-    //     <button onClick={addJob}>Add</button>
-    //   </div>
-    // </Box>
+    
 
-    <MaterialTable
-      title="User list from API"
-      columns={columns}
-      data={jobs}
-      //  icons={tableIcons}
-      editable={{
-        onRowUpdate: (newData, oldData) =>
-          new Promise((resolve) => {
-            handleRowUpdate(newData, oldData, resolve);
-          }),
-        onRowAdd: (newData) =>
-          new Promise((resolve) => {
-            handleRowAdd(newData, resolve);
-          }),
-        onRowDelete: (oldData) =>
-          new Promise((resolve) => {
-            handleRowDelete(oldData, resolve);
-          }),
-      }}
-    />
+     <Box
+     
+       sx={{
+         height: "450px",
+         width: "120vh",
+         margin: "20vh",
+         marginLeft: "50vh",
+       }}
+     >
+         <DataGrid
+        editMode="row"
+        rows={jobs}
+        columns={columns}
+        experimentalFeatures={{ newEditingApi: true }}
+        onRowEditStop={(params, event) => handleCellChange(params, event.target.value)}
+        // checkboxSelection
+        // onSelectionModelChange={(ids) => {
+        //   setSelectionModel(ids);
+        // }}
+       // isRowSelectable={(params) => params.row.id > 18}
+       checkboxSelection
+        selectionModel={selectionModel}
+        hideFooterSelectedRowCount
+        onSelectionModelChange={(selection) => {
+          if (selection.length > 1) {
+            const selectionSet = new Set(selectionModel);
+            const result = selection.filter((s) => !selectionSet.has(s));
+
+            setSelectionModel(result);
+            console.log(selectionModel);
+          } else {
+            setSelectionModel(selection);
+            console.log(selectionModel);
+          }
+        }}
+      />
+       <div className="create-button">
+         <button onClick={returnHomePage}>Home</button>
+         <button onClick={addJob}>Add</button>
+         <Icon baseClassName="fas" className="fa-plus-circle" fontSize="small" />
+       </div>
+       <div >
+         <button onClick={addJob}>delete</button>
+       </div>
+     </Box>
+
   );
 }
 export default ListJob;
