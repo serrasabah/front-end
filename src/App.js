d6fbc11d5198b4e4dff302189f739a3de9db6535
serrasabah <69539523+serrasabah@users.noreply.github.com>
2022-08-13 11:23:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,20 +7,94 @@ import ListJob from "./components/ListJob";
 import AddJob from "./components/AddJob";
 import ListUsers from "./components/ListUsers";
 import Login from "./components/Login";
-
+import { ToastContainer } from "react-toastify";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/AdminPage" element={<AdminPage />} />
-        <Route path="/NewUser" element={<NewUser />} />
-        <Route path="/ListJob" element={<ListJob />} />
-        <Route path="/ListUsers" element={<ListUsers />} />
-        <Route path="/AddJob" element={<AddJob />} />
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/AdminPage" element={<AdminPage />} />
+          <Route path="/NewUser" element={<NewUser />} />
+          <Route path="/ListJob" element={<ListJob />} />
+          <Route path="/ListUsers" element={<ListUsers />} />
+          <Route path="/AddJob" element={<AddJob />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+
+
+
+
   );
 }
 
 export default App;
+
+
+// import "react-toastify/dist/ReactToastify.css";
+// import "./App.css";
+// import AdminPage from "./components/AdminPage";
+// import NewUser from "./components/NewUser";
+// import ListJob from "./components/ListJob";
+// import AddJob from "./components/AddJob";
+// import ListUsers from "./components/ListUsers";
+// import Login from "./components/Login";
+// import { ToastContainer } from "react-toastify";
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// //import PrivateRoutes from './components/PrivateRoutes'
+// function App() {
+//   return (
+//     <>
+//       <ToastContainer
+//         position="top-right"
+//         autoClose={5000}
+//         hideProgressBar={false}
+//         newestOnTop={false}
+//         closeOnClick
+//         rtl={false}
+//         pauseOnFocusLoss
+//         draggable
+//         pauseOnHover
+//       />
+//       <BrowserRouter>
+//         <Routes>
+//           {/* <Route element={<PrivateRoutes />}>
+//             <Route element={<AdminPage />} path="/AdminPage" exact />
+//             <Route element={<ListJob />} path="/ListJob" />
+//             <Route path="/NewUser" element={<NewUser />} />
+//             <Route path="/ListUsers" element={<ListUsers />} />
+//             <Route path="/AddJob" element={<AddJob />} />
+//             <Route path="/" element={<AdminPage />} /> 
+//           </Route>
+//           <Route element={<Login />} path="/Login" /> */}
+//           <Route path="/Login" component={Login} />
+//           <Route path="/AdminPage" element={<AdminPage />} />
+//           <Route path="/NewUser" element={<NewUser />} />
+//           <Route path="/ListJob" element={<ListJob />} />
+//           <Route path="/ListUsers" element={<ListUsers />} />
+//           <Route path="/AddJob" element={<AddJob />} />
+//           <Route path="/" element={<Login />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+
+
+
+
+//   );
+// }
+
+// export default App;
