49fdd5c2b90a2c197d95c239ad4308ce247b6d80
serrasabah <69539523+serrasabah@users.noreply.github.com>
2022-08-18 09:10:33 +0300

    Update UserApi.js

--- src/components/UserApi.js
+++ src/components/UserApi.js
@@ -5,7 +5,15 @@ export class UserApi {
     return axios.get("/user");
   }
 
-  // addUsers(formState) {
-  //   return axios.post("/user", formState);
-  // }
+  addUsers(formState) {
+    return axios.post("/user", formState);
+  }
+
+  updateUsers(id, newData) {
+    return axios.put("/user/" + id, newData);
+  }
+
+  deleteUser(id) {
+    return axios.delete("/user/" + id);
+  }
 }
